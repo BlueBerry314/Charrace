79ae2aac1416842d2020750ca3eb62cc17da6b5c
Marius Urbonas <marius.urbonas.edu@gmail.com>
2015-11-28 23:55:42 +0000

    Added basic functions for the server

--- Assets/Flask.py
+++ Assets/Flask.py
@@ -1,7 +1,11 @@
 __author__ = '1m2i3_000'
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, g, redirect, url_for, abort
 import json
 import requests
+import xmltodict
+import os
+from sqlite3 import dbapi2 as sqlite3
+
 app = Flask(__name__)
 
 Charity  = {"290626":"Suited to Success Inc."
@@ -9,7 +13,51 @@ Charity  = {"290626":"Suited to Success Inc."
            ,"2050":"The Demo Charity (JustGiving Demo)"
            ,"186234":"Al-khair"}
 
-api_key = '07074f97'
+api_key = '2e8046d8'
+
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route("/")
 def index():
@@ -25,11 +73,16 @@ def getCharities():
 
 @app.route('/api/post/<charity_id>')
 def getId (charity_id):
-    pass
+    testy = get_Charity_Details(charity_id)
+    return testy
 
 def get_Charity_Details(_charity_id):
-    url = 'https://v3-sandbox.justgiving.com/donation/direct/'
-    details = requests.get('https://api.justgiving.com/%s/v1/charity/search?q=&charityid=%s'(api_key,_charity_id))
+    url = 'https://v3-sandbox.justgiving.com/donation/direct/charity/%s' % (_charity_id)
+    details = requests.get('https://api.justgiving.com/%s/v1/charity/search?q=&charityid=%s' % (api_key,_charity_id))
+    obj = xmltodict.parse(details.content)
+    description = obj['charitySearch']['charitySearchResults']['charitySearchResult']['description']
+    name = obj['charitySearch']['charitySearchResults']['charitySearchResult']['charityDisplayName']
+    return jsonify(url=url,name=name,description=description)
 
 
 
